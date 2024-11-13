019bd6daefb8ab0453fc03ad72ba139a57743fc8
Balora308 <rost.lysak@icloud.com>
2024-11-13 20:49:50 +0200

    fix v1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { TelegramService } from './telegram/telegram/telegram.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   const telegramService = app.get(TelegramService);
 
